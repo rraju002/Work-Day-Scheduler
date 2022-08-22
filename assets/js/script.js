519f35fc1dedf17e58edcb706ba6b988b92b293f
Rhowen01 <rraju002@ucr.edu>
2022-08-22 01:07:58 -0700

    added a better looking front end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,85 @@
-// Show today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+/*
+ * The function currentDate() is called, which uses the moment.js library to get the current date and
+ * time, and then displays it in the #currentDay div.
+ */
+function currentDate() {
+    var today = moment();
+    $("#currentDay").text(today.format("MMM Do, YYYY"));
+}
+currentDate();
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get values of description(s) in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+//-----------------------------------------
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
+var container = $('.container');
+var hours = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+//for loop to grab all the arrays time and display all those to the calendar
+/* I want to dynamically create elements that will then be displayed
+on the screen, i want to add those new elements to the container
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+use moment.js to check if the current time in hours is in the past present or future and cordinate it by colors */
 
-            // Check time and add class for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
+var momentToday = moment().format('H');
+var momentHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var id = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-            }
-        })
-    }
+//-----------------------------------------
 
-    // Get item from local storage 
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+function displayRows() {
+    for (var i = 0; i < hours.length; i++) {
 
-    timeTracker();
-})
+        var row = $('<div class="row">');
+
+        var dailyTime = $('<div>');
+        dailyTime.attr('class', 'col-2 col-sm-2 col-md-2 dailyTime');
+        dailyTime.append('<p class="time-block">' + hours[i] + '</p>');
+
+        var mainInput = $('<div>');
+        mainInput.attr('class', 'col-7 col-sm-8 col-md-8 mainInput');
+        mainInput.append('<textarea class="input" placeholder="Type Here"></textarea>');
+
+        var saveBtn = $('<div>');
+        saveBtn.attr('class', 'col-1 col-sm-1 col-md-1');
+        saveBtn.append('<button class="saveBtn"><i class="fa-solid fa-lock">');
+
+        container.append(row);
+        row.append(dailyTime);
+        row.append(mainInput);
+        row.append(saveBtn);
+        saveBtn.attr('id', id[i]);
+        mainInput.attr('id', id[i])
+
+        if (momentToday > momentHours[i]) {
+            console.log('time is in the past ' + momentHours[i]);
+            row.css('background-color', '#B9B9B9');
+        }
+        if (momentToday === momentHours[i]) {
+            console.log('time is present ' + momentHours[i]);
+            row.css('background-color', 'white');
+
+        }
+        if (momentToday < momentHours[i]) {
+            console.log('time is in the future ' + momentHours[i]);
+            row.css('background-color', '#ECBCFF');
+        }
+    };
+}
+displayRows();
+
+//-----------------------------------------
+//WHEN I click into a timeblock
+//THEN I can enter an event
+//When I click into mainInput and go to the textarea 
+//I want an event listener on the button to save my text input
+//-----------------------------------------
+
+// Saving the text input to local storage and then displaying it in the main input.
+$(".saveBtn").on('click', function() {
+    var textInput = $('.input').val();
+    localStorage.setItem('textInput', textInput);
+    var textDisplay = localStorage.getItem('textInput');
+   mainInput.text = textDisplay.val()
+});
+
+//-----------------------------------------
